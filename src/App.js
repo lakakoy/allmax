348605365f03d982443bdeb7d9e6fb0845ca9a92
Daniil <mrmrkda@gmail.com>
2019-06-26 16:11:32 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <GlobalStyle />
+      Allmax
+    </Container>
+  )
 }
 
-export default App;
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Noto+Sans');
+  * { box-sizing: border-box; }
+  body {
+    margin: 0;
+    font-family: 'Noto Sans', sans-serif;
+  }
+`
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+`
